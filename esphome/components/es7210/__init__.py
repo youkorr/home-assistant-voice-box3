099c6a0db2dffe6eb9592ef63aadb6c6c5ea46cf
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-21 23:25:46 +0100

    Update __init__.py

--- esphome/components/es7210/__init__.py
+++ esphome/components/es7210/__init__.py
@@ -1,19 +1,19 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import i2c, microphone
-from esphome.const import CONF_ID
+    import esphome.config_validation as cv
+    from esphome.components import i2c, microphone
+    from esphome.const import CONF_ID
 
-DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["microphone"]
+    DEPENDENCIES = ["i2c"]
+    AUTO_LOAD = ["microphone"]
 
-es7210_ns = cg.esphome_ns.namespace("es7210")
-ES7210 = es7210_ns.class_("ES7210", i2c.I2CDevice, cg.Component)
+    es7210_ns = cg.esphome_ns.namespace("es7210")
+    ES7210 = es7210_ns.class_("ES7210", i2c.I2CDevice, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ES7210),
-}).extend(i2c.i2c_device_schema(0x40)).extend(cv.COMPONENT_SCHEMA)
+    CONFIG_SCHEMA = cv.Schema({
+        cv.GenerateID(): cv.declare_id(ES7210),
+    }).extend(i2c.i2c_device_schema(0x40)).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield i2c.register_i2c_device(var, config)
+    async def to_code(config):
+        var = cg.new_Pvariable(config[CONF_ID])
+        await cg.register_component(var, config)
+        await i2c.register_i2c_device(var, config)
