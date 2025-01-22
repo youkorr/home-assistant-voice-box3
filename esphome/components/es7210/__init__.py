e93a38b05a664683e00529fba02ba8134be72577
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-22 23:20:50 +0100

    Update __init__.py

--- esphome/components/es7210/__init__.py
+++ esphome/components/es7210/__init__.py
@@ -1,16 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, microphone
+from esphome.components import i2c
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["microphone"]
 
-# Déclarer le composant et ses fichiers source
 es7210_ns = cg.esphome_ns.namespace("es7210")
 ES7210 = es7210_ns.class_("ES7210", i2c.I2CDevice, cg.Component)
 
-# Configuration du schéma YAML
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ES7210),
 }).extend(i2c.i2c_device_schema(0x40)).extend(cv.COMPONENT_SCHEMA)
@@ -18,11 +15,14 @@ CONFIG_SCHEMA = cv.Schema({
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     
-    # Ajouter explicitement les fichiers source C++
-    cg.add_library("espressif/esp32-camera", None)  # Exemple de dépendance externe (si nécessaire)
-    cg.add_build_flag("-DUSE_ES7210")  
-    cg.add_define("USE_ES7210")  
-    
+    # Ajouter les fichiers nécessaires
+    cg.add_library("es7210", None)  # Facultatif si vous avez une bibliothèque externe
+    cg.add_include("es7210/es7210_idf.h")
+    cg.add_include("es7210/es7210.h")
+    cg.add_source("es7210/es7210_idf.cpp")
+    cg.add_source("es7210/es7210.cpp")
+
     # Enregistrer le composant et le périphérique I2C
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
+
