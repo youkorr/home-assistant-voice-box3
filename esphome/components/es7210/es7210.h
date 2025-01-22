593f8a426b1846b3be4c95a6d6c0c54f605b977f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-22 04:01:22 +0100

    Update es7210.h

--- esphome/components/es7210/es7210.h
+++ esphome/components/es7210/es7210.h
@@ -1,24 +1,17 @@
 #pragma once
-
 #include "esphome.h"
-#include "esphome/core/component.h"
-#include "esphome/components/i2c/i2c.h"
 
-namespace esphome {
-namespace es7210 {
+class ES7210 : public Component {
+public:
+    void setup() override;
+    void loop() override;
 
-class ES7210 : public i2c::I2CDevice, public Component {
- public:
-  void setup() override;
-  void configure(uint8_t sample_rate, uint8_t gain);
-  void start_recording();
-  void stop_recording();
+    void set_i2c_address(uint8_t address);
+    void set_sample_rate(uint32_t sample_rate);
+    void set_bits_per_sample(uint8_t bits_per_sample);
 
- private:
-  bool is_recording = false;
-  void write_register(uint8_t reg, uint8_t value);
-  uint8_t read_register(uint8_t reg);
+private:
+    uint8_t i2c_address;
+    uint32_t sample_rate;
+    uint8_t bits_per_sample;
 };
-
-}  // namespace es7210
-}  // namespace esphome
