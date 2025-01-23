0d509196da4a8cf0c82c3862244df9fbadbfea6c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-23 04:40:26 +0100

    Update media_player.py

--- esphome/components/custom_audio/media_player.py
+++ esphome/components/custom_audio/media_player.py
@@ -6,7 +6,9 @@ from esphome.const import CONF_ID, CONF_I2S_AUDIO_ID
 DEPENDENCIES = ['i2s_audio']
 AUTO_LOAD = ['media_player']
 
+# Namespace DOIT correspondre au nom du composant
 custom_audio_ns = cg.esphome_ns.namespace('custom_audio')
+
 CustomAudioMediaPlayer = custom_audio_ns.class_(
     'CustomAudioMediaPlayer',
     media_player.MediaPlayer,
@@ -21,9 +23,10 @@ CONFIG_SCHEMA = cv.platform_schema(
     }).extend(cv.COMPONENT_SCHEMA)
 )
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield media_player.register_media_player(var, config)
-    i2s = yield cg.get_variable(config[CONF_I2S_AUDIO_ID])
+    await cg.register_component(var, config)
+    await media_player.register_media_player(var, config)
+    
+    i2s = await cg.get_variable(config[CONF_I2S_AUDIO_ID])
     cg.add(var.set_i2s_output(i2s))
