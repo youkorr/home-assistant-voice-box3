92a2c8887ffc7d8d8f31a3097c68f78fe2bb2b37
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-23 04:04:30 +0100

    Update custom_audio.h

--- esphome/components/custom_audio/custom_audio.h
+++ esphome/components/custom_audio/custom_audio.h
@@ -1,21 +1,27 @@
 #pragma once
 #include "esphome.h"
-#include "esphome/components/media_player/media_player.h"
+#include <Audio.h>
 
 namespace esphome {
 namespace custom_audio {
 
-class CustomAudioMediaPlayer : public media_player::MediaPlayer, public Component {
+class CustomAudioMediaPlayer : public Component, 
+                             public media_player::MediaPlayer,
+                             public i2s_audio::I2SAudioOutput {
  public:
+  void set_i2s_output(i2s_audio::I2SAudioOutput *output) { 
+    this->output_ = output; 
+  }
+  
   void setup() override;
+  void loop() override;
+  
+  // Implémenter les méthodes MediaPlayer
+  media_player::MediaPlayerTraits get_traits() override;
   void control(const media_player::MediaPlayerCall &call) override;
-  media_player::MediaPlayerTraits traits() override;
 
  protected:
-  void play();
-  void pause();
-  void stop();
-  void set_volume(float volume);
+  i2s_audio::I2SAudioOutput *output_;
 };
 
 }  // namespace custom_audio
