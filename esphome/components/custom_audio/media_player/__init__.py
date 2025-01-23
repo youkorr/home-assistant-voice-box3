c80981fba893bb04b67bb7ccc93aa32bdb6f0ec4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-23 04:23:53 +0100

    Update __init__.py

--- esphome/components/custom_audio/media_player/__init__.py
+++ esphome/components/custom_audio/media_player/__init__.py
@@ -1,1 +1,7 @@
+from esphome import pins
+from esphome.components.media_player import MediaPlayer, MediaPlayerTraits
 
+# Lien entre Python et C++
+CODEOWNERS = ["@votre_nom"]
+DEPENDENCIES = ["custom_audio"]
+IS_PLATFORM_COMPONENT = True
