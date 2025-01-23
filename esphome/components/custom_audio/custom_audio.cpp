17faaa560e344ab5e150ffcc0d6fd658dc737fd2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-23 01:48:33 +0100

    Update custom_audio.cpp

--- esphome/components/custom_audio/custom_audio.cpp
+++ esphome/components/custom_audio/custom_audio.cpp
@@ -4,44 +4,52 @@ namespace esphome {
 namespace custom_audio {
 
 void CustomAudioMediaPlayer::setup() {
-  // Initialisation de votre lecteur audio
+  // Initialisation du lecteur audio
+  ESP_LOGCONFIG(TAG, "Setting up Custom Audio Media Player...");
 }
 
 void CustomAudioMediaPlayer::control(const media_player::MediaPlayerCall &call) {
   if (call.get_media_url().has_value()) {
     // Jouer une URL ou un fichier
+    ESP_LOGD(TAG, "Playing media: %s", call.get_media_url()->c_str());
+    this->play();
   }
   if (call.get_volume().has_value()) {
-    set_volume(*call.get_volume());
+    // Régler le volume
+    this->set_volume(*call.get_volume());
   }
   if (call.get_command() == media_player::MEDIA_PLAYER_COMMAND_PLAY) {
-    play();
+    this->play();
   } else if (call.get_command() == media_player::MEDIA_PLAYER_COMMAND_PAUSE) {
-    pause();
+    this->pause();
   } else if (call.get_command() == media_player::MEDIA_PLAYER_COMMAND_STOP) {
-    stop();
+    this->stop();
   }
 }
 
 media_player::MediaPlayerTraits CustomAudioMediaPlayer::traits() {
   auto traits = media_player::MediaPlayerTraits();
-  traits.set_supports_pause(true);
+  traits.set_supports_pause(true);  // Indique que le lecteur prend en charge la pause
   return traits;
 }
 
 void CustomAudioMediaPlayer::play() {
+  ESP_LOGD(TAG, "Playing audio...");
   // Implémentez la logique pour jouer l'audio
 }
 
 void CustomAudioMediaPlayer::pause() {
+  ESP_LOGD(TAG, "Pausing audio...");
   // Implémentez la logique pour mettre en pause l'audio
 }
 
 void CustomAudioMediaPlayer::stop() {
+  ESP_LOGD(TAG, "Stopping audio...");
   // Implémentez la logique pour arrêter l'audio
 }
 
 void CustomAudioMediaPlayer::set_volume(float volume) {
+  ESP_LOGD(TAG, "Setting volume to: %.2f", volume);
   // Implémentez la logique pour régler le volume
 }
 
