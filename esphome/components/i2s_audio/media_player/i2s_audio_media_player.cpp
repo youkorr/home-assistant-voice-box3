9dfbd6c64a8f9b83b389578bd4d3c839b977348f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-23 23:56:22 +0100

    Update i2s_audio_media_player.cpp

--- esphome/components/i2s_audio/media_player/i2s_audio_media_player.cpp
+++ esphome/components/i2s_audio/media_player/i2s_audio_media_player.cpp
@@ -1,6 +1,6 @@
 #include "i2s_audio_media_player.h"
 
-#ifdef USE_ESP32_FRAMEWORK_ARDUINO
+#ifdef USE_ESP32
 
 #include "esphome/core/log.h"
 
@@ -17,15 +17,10 @@ void I2SAudioMediaPlayer::control(const media_player::MediaPlayerCall &call) {
   }
   if (call.get_media_url().has_value()) {
     this->current_url_ = call.get_media_url();
-    if (this->i2s_state_ != I2S_STATE_STOPPED && this->audio_ != nullptr) {
-      if (this->audio_->isRunning()) {
-        this->audio_->stopSong();
-      }
-      this->audio_->connecttohost(this->current_url_.value().c_str());
-      this->state = play_state;
-    } else {
-      this->start();
+    if (this->i2s_state_ != I2S_STATE_STOPPED) {
+      this->stop();
     }
+    this->start();
   }
 
   if (play_state == media_player::MEDIA_PLAYER_STATE_ANNOUNCING) {
@@ -64,34 +59,6 @@ void I2SAudioMediaPlayer::control(const media_player::MediaPlayerCall &call) {
       default:
         break;
     }
-    if (this->i2s_state_ != I2S_STATE_RUNNING) {
-      return;
-    }
-    switch (call.get_command().value()) {
-      case media_player::MEDIA_PLAYER_COMMAND_PLAY:
-        if (!this->audio_->isRunning())
-          this->audio_->pauseResume();
-        this->state = play_state;
-        break;
-      case media_player::MEDIA_PLAYER_COMMAND_PAUSE:
-        if (this->audio_->isRunning())
-          this->audio_->pauseResume();
-        this->state = media_player::MEDIA_PLAYER_STATE_PAUSED;
-        break;
-      case media_player::MEDIA_PLAYER_COMMAND_STOP:
-        this->stop();
-        break;
-      case media_player::MEDIA_PLAYER_COMMAND_TOGGLE:
-        this->audio_->pauseResume();
-        if (this->audio_->isRunning()) {
-          this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
-        } else {
-          this->state = media_player::MEDIA_PLAYER_STATE_PAUSED;
-        }
-        break;
-      default:
-        break;
-    }
   }
   this->publish_state();
 }
@@ -113,8 +80,7 @@ void I2SAudioMediaPlayer::unmute_() {
   this->muted_ = false;
 }
 void I2SAudioMediaPlayer::set_volume_(float volume, bool publish) {
-  if (this->audio_ != nullptr)
-    this->audio_->setVolume(remap<uint8_t, float>(volume, 0.0f, 1.0f, 0, 21));
+  // Implémentez la logique pour régler le volume ici
   if (publish)
     this->volume = volume;
 }
@@ -141,10 +107,9 @@ void I2SAudioMediaPlayer::loop() {
 }
 
 void I2SAudioMediaPlayer::play_() {
-  this->audio_->loop();
+  // Implémentez la logique pour lire l'audio ici
   if ((this->state == media_player::MEDIA_PLAYER_STATE_PLAYING ||
-       this->state == media_player::MEDIA_PLAYER_STATE_ANNOUNCING) &&
-      !this->audio_->isRunning()) {
+       this->state == media_player::MEDIA_PLAYER_STATE_ANNOUNCING)) {
     this->stop();
   }
 }
@@ -155,32 +120,21 @@ void I2SAudioMediaPlayer::start_() {
     return;  // Waiting for another i2s to return lock
   }
 
-#if SOC_I2S_SUPPORTS_DAC
-  if (this->internal_dac_mode_ != I2S_DAC_CHANNEL_DISABLE) {
-    this->audio_ = make_unique<Audio>(true, this->internal_dac_mode_, this->parent_->get_port());
-  } else {
-#endif
-    this->audio_ = make_unique<Audio>(false, 3, this->parent_->get_port());
+  i2s_pin_config_t pin_config = this->parent_->get_pin_config();
+  pin_config.data_out_num = this->dout_pin_;
+  i2s_set_pin(this->parent_->get_port(), &pin_config);
 
-    i2s_pin_config_t pin_config = this->parent_->get_pin_config();
-    pin_config.data_out_num = this->dout_pin_;
-    i2s_set_pin(this->parent_->get_port(), &pin_config);
-
-    this->audio_->setI2SCommFMT_LSB(this->i2s_comm_fmt_lsb_);
-    this->audio_->forceMono(this->external_dac_channels_ == 1);
-    if (this->mute_pin_ != nullptr) {
-      this->mute_pin_->setup();
-      this->mute_pin_->digital_write(false);
-    }
-#if SOC_I2S_SUPPORTS_DAC
+  if (this->mute_pin_ != nullptr) {
+    this->mute_pin_->setup();
+    this->mute_pin_->digital_write(false);
   }
-#endif
 
   this->i2s_state_ = I2S_STATE_RUNNING;
   this->high_freq_.start();
-  this->audio_->setVolume(remap<uint8_t, float>(this->volume, 0.0f, 1.0f, 0, 21));
+  this->set_volume_(this->volume, false);
+
   if (this->current_url_.has_value()) {
-    this->audio_->connecttohost(this->current_url_.value().c_str());
+    // Implémentez la logique pour lire l'URL audio ici
     this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
     if (this->is_announcement_) {
       this->state = media_player::MEDIA_PLAYER_STATE_ANNOUNCING;
@@ -199,12 +153,6 @@ void I2SAudioMediaPlayer::stop() {
   this->i2s_state_ = I2S_STATE_STOPPING;
 }
 void I2SAudioMediaPlayer::stop_() {
-  if (this->audio_->isRunning()) {
-    this->audio_->stopSong();
-    return;
-  }
-
-  this->audio_ = nullptr;
   this->current_url_ = {};
   this->parent_->unlock();
   this->i2s_state_ = I2S_STATE_STOPPED;
@@ -227,32 +175,12 @@ void I2SAudioMediaPlayer::dump_config() {
     ESP_LOGCONFIG(TAG, "Audio failed to initialize!");
     return;
   }
-#if SOC_I2S_SUPPORTS_DAC
-  if (this->internal_dac_mode_ != I2S_DAC_CHANNEL_DISABLE) {
-    switch (this->internal_dac_mode_) {
-      case I2S_DAC_CHANNEL_LEFT_EN:
-        ESP_LOGCONFIG(TAG, "  Internal DAC mode: Left");
-        break;
-      case I2S_DAC_CHANNEL_RIGHT_EN:
-        ESP_LOGCONFIG(TAG, "  Internal DAC mode: Right");
-        break;
-      case I2S_DAC_CHANNEL_BOTH_EN:
-        ESP_LOGCONFIG(TAG, "  Internal DAC mode: Left & Right");
-        break;
-      default:
-        break;
-    }
-  } else {
-#endif
-    ESP_LOGCONFIG(TAG, "  External DAC channels: %d", this->external_dac_channels_);
-    ESP_LOGCONFIG(TAG, "  I2S DOUT Pin: %d", this->dout_pin_);
-    LOG_PIN("  Mute Pin: ", this->mute_pin_);
-#if SOC_I2S_SUPPORTS_DAC
-  }
-#endif
+  ESP_LOGCONFIG(TAG, "  External DAC channels: %d", this->external_dac_channels_);
+  ESP_LOGCONFIG(TAG, "  I2S DOUT Pin: %d", this->dout_pin_);
+  LOG_PIN("  Mute Pin: ", this->mute_pin_);
 }
 
 }  // namespace i2s_audio
 }  // namespace esphome
 
-#endif  // USE_ESP32_FRAMEWORK_ARDUINO
+#endif  // USE_ESP32
