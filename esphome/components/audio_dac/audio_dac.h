add0197d4d11cda2de1f9d29112b02fa68cb2715
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 03:21:01 +0100

    Update audio_dac.h

--- esphome/components/audio_dac/audio_dac.h
+++ esphome/components/audio_dac/audio_dac.h
@@ -7,18 +7,18 @@ namespace audio_dac {
 
 class AudioDAC {
  public:
-  virtual void start() = 0;
-  virtual void stop() = 0;
-  virtual void write(const uint8_t *data, size_t len) = 0;
-  
-  virtual bool set_mute_off() = 0;
-  virtual bool set_mute_on() = 0;
-  virtual bool set_volume(float volume) = 0;
-  virtual bool is_muted() = 0;
-  virtual float volume() = 0;
+  virtual void start() = 0;  // Démarrer le DAC audio
+  virtual void stop() = 0;   // Arrêter le DAC audio
+  virtual void write(const uint8_t *data, size_t len) = 0;  // Écrire des données audio
+
+  virtual bool set_mute_off() = 0;  // Désactiver la mise en sourdine
+  virtual bool set_mute_on() = 0;   // Activer la mise en sourdine
+  virtual bool set_volume(float volume) = 0;  // Régler le volume
+  virtual bool is_muted() = 0;  // Vérifier si le son est en sourdine
+  virtual float volume() = 0;   // Obtenir le volume actuel
 
  protected:
-  bool is_muted_{false};
+  bool is_muted_{false};  // État de la mise en sourdine
 };
 
 }  // namespace audio_dac
