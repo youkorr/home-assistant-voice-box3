171de26cddcdbe2f02b6ffb191fd7ad4c4912f1f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 21:56:34 +0100

    Update __init__.py

--- esphome/components/media_player/__init__.py
+++ esphome/components/media_player/__init__.py
@@ -11,20 +11,20 @@ IS_PLATFORM_COMPONENT = True
 media_player_ns = cg.esphome_ns.namespace("media_player")
 MediaPlayer = media_player_ns.class_("MediaPlayer", cg.Component)
 
-# Configuration schema
+# Configuration schema MODIFIÉE
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MediaPlayer),
     cv.Required(CONF_NAME): cv.string,
-    cv.Required("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudio),
+    # Utilisez directement l'ID du composant i2s_audio sans référence de classe
+    cv.Required("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),  # <-- Changement clé ici
 }).extend(cv.COMPONENT_SCHEMA)
 
-# Function to generate the code
 @coroutine_with_priority(100.0)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.set_name(config[CONF_NAME]))
     
-    # Bind i2s_audio to media_player
+    # Lier i2s_audio via son ID générique
     i2s_audio_var = await cg.get_variable(config["i2s_audio_id"])
     cg.add(var.set_i2s_audio(i2s_audio_var))
