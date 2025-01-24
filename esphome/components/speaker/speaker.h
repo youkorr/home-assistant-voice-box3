ef825fa939b246b79adfb6e1a63a24d8d1703f50
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 03:21:40 +0100

    Update speaker.h

--- esphome/components/speaker/speaker.h
+++ esphome/components/speaker/speaker.h
@@ -9,10 +9,10 @@
 #endif
 
 #include "esphome/core/defines.h"
-
 #include "esphome/components/audio/audio.h"
+
 #ifdef USE_AUDIO_DAC
-#include "esphome/components/audio_dac/audio_dac.h"
+#include "esphome/components/audio_dac/audio_dac.h"  // Assurez-vous que ce fichier existe et est correct
 #endif
 
 namespace esphome {
@@ -29,31 +29,18 @@ class Speaker {
  public:
 #ifdef USE_ESP32
   /// @brief Plays the provided audio data.
-  /// If the speaker component doesn't implement this method, it falls back to the play method without this parameter.
-  /// @param data Audio data in the format specified by ``set_audio_stream_info`` method.
-  /// @param length The length of the audio data in bytes.
-  /// @param ticks_to_wait The FreeRTOS ticks to wait before writing as much data as possible to the ring buffer.
-  /// @return The number of bytes that were actually written to the speaker's internal buffer.
   virtual size_t play(const uint8_t *data, size_t length, TickType_t ticks_to_wait) {
     return this->play(data, length);
   }
 #endif
 
   /// @brief Plays the provided audio data.
-  /// If the audio stream is not the default defined in "esphome/core/audio.h" and the speaker component implements it,
-  /// then this should be called after calling ``set_audio_stream_info``.
-  /// @param data Audio data in the format specified by ``set_audio_stream_info`` method.
-  /// @param length The length of the audio data in bytes.
-  /// @return The number of bytes that were actually written to the speaker's internal buffer.
   virtual size_t play(const uint8_t *data, size_t length) = 0;
 
   size_t play(const std::vector<uint8_t> &data) { return this->play(data.data(), data.size()); }
 
   virtual void start() = 0;
   virtual void stop() = 0;
-  // In compare between *STOP()* and *FINISH()*; *FINISH()* will stop after emptying the play buffer,
-  // while *STOP()* will break directly.
-  // When finish() is not implemented on the platform component it should just do a normal stop.
   virtual void finish() { this->stop(); }
 
   virtual bool has_buffered_data() const = 0;
@@ -61,8 +48,7 @@ class Speaker {
   bool is_running() const { return this->state_ == STATE_RUNNING; }
   bool is_stopped() const { return this->state_ == STATE_STOPPED; }
 
-  // Volume control is handled by a configured audio dac component. Individual speaker components can
-  // override and implement in software if an audio dac isn't available.
+  // Volume control
   virtual void set_volume(float volume) {
     this->volume_ = volume;
 #ifdef USE_AUDIO_DAC
@@ -74,6 +60,7 @@ class Speaker {
 
   float get_volume() { return this->volume_; }
 
+  // Mute control
   virtual void set_mute_state(bool mute_state) {
     this->mute_state_ = mute_state;
 #ifdef USE_AUDIO_DAC
@@ -104,7 +91,7 @@ class Speaker {
   bool mute_state_{false};
 
 #ifdef USE_AUDIO_DAC
-  audio_dac::AudioDAC *audio_dac_{nullptr};
+  audio_dac::AudioDAC *audio_dac_{nullptr};  // Déclaration conditionnelle de audio_dac_
 #endif
 };
 
