d736d3489ab9cca83c3e8c8b45c67f974465854e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 01:41:39 +0100

    Update es8311.h

--- esphome/components/audio_dac/es8311.h
+++ esphome/components/audio_dac/es8311.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
 
@@ -7,14 +8,8 @@ namespace es8311 {
 class ES8311Component : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
-  void start();
-  void stop();
-  void write(const uint8_t *data, size_t len);
-
   void write_register(uint8_t reg, uint8_t value);
-
- protected:
-  // Add any necessary protected members
+  // ... (autres méthodes)
 };
 
 }  // namespace es8311
