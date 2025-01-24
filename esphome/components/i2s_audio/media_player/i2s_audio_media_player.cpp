68fde8d543ed387c594337a46fe333b10e4449de
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 12:50:34 +0100

    Update i2s_audio_media_player.cpp

--- esphome/components/i2s_audio/media_player/i2s_audio_media_player.cpp
+++ esphome/components/i2s_audio/media_player/i2s_audio_media_player.cpp
@@ -1,186 +1,152 @@
-#include "i2s_audio_media_player.h"
-
-#ifdef USE_ESP32
+#include "media_player.h"
 
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace i2s_audio {
+namespace media_player {
 
-static const char *const TAG = "audio";
+static const char *const TAG = "media_player";
 
-void I2SAudioMediaPlayer::control(const media_player::MediaPlayerCall &call) {
-  media_player::MediaPlayerState play_state = media_player::MEDIA_PLAYER_STATE_PLAYING;
-  if (call.get_announcement().has_value()) {
-    play_state = call.get_announcement().value() ? media_player::MEDIA_PLAYER_STATE_ANNOUNCING
-                                                 : media_player::MEDIA_PLAYER_STATE_PLAYING;
+const char *media_player_state_to_string(MediaPlayerState state) {
+  switch (state) {
+    case MEDIA_PLAYER_STATE_IDLE:
+      return "IDLE";
+    case MEDIA_PLAYER_STATE_PLAYING:
+      return "PLAYING";
+    case MEDIA_PLAYER_STATE_PAUSED:
+      return "PAUSED";
+    case MEDIA_PLAYER_STATE_ANNOUNCING:
+      return "ANNOUNCING";
+    case MEDIA_PLAYER_STATE_NONE:
+    default:
+      return "UNKNOWN";
   }
-  if (call.get_media_url().has_value()) {
-    this->current_url_ = call.get_media_url();
-    if (this->i2s_state_ != I2S_STATE_STOPPED) {
-      this->stop();
-    }
-    this->start();
-  }
-
-  if (play_state == media_player::MEDIA_PLAYER_STATE_ANNOUNCING) {
-    this->is_announcement_ = true;
-  }
-
-  if (call.get_volume().has_value()) {
-    this->volume = call.get_volume().value();
-    this->set_volume_(volume);
-    this->unmute_();
-  }
-  if (call.get_command().has_value()) {
-    switch (call.get_command().value()) {
-      case media_player::MEDIA_PLAYER_COMMAND_MUTE:
-        this->mute_();
-        break;
-      case media_player::MEDIA_PLAYER_COMMAND_UNMUTE:
-        this->unmute_();
-        break;
-      case media_player::MEDIA_PLAYER_COMMAND_VOLUME_UP: {
-        float new_volume = this->volume + 0.1f;
-        if (new_volume > 1.0f)
-          new_volume = 1.0f;
-        this->set_volume_(new_volume);
-        this->unmute_();
-        break;
-      }
-      case media_player::MEDIA_PLAYER_COMMAND_VOLUME_DOWN: {
-        float new_volume = this->volume - 0.1f;
-        if (new_volume < 0.0f)
-          new_volume = 0.0f;
-        this->set_volume_(new_volume);
-        this->unmute_();
-        break;
-      }
-      default:
-        break;
-    }
-  }
-  this->publish_state();
 }
 
-void I2SAudioMediaPlayer::mute_() {
-  if (this->mute_pin_ != nullptr) {
-    this->mute_pin_->digital_write(true);
+const char *media_player_command_to_string(MediaPlayerCommand command) {
+  switch (command) {
+    case MEDIA_PLAYER_COMMAND_PLAY:
+      return "PLAY";
+    case MEDIA_PLAYER_COMMAND_PAUSE:
+      return "PAUSE";
+    case MEDIA_PLAYER_COMMAND_STOP:
+      return "STOP";
+    case MEDIA_PLAYER_COMMAND_MUTE:
+      return "MUTE";
+    case MEDIA_PLAYER_COMMAND_UNMUTE:
+      return "UNMUTE";
+    case MEDIA_PLAYER_COMMAND_TOGGLE:
+      return "TOGGLE";
+    case MEDIA_PLAYER_COMMAND_VOLUME_UP:
+      return "VOLUME_UP";
+    case MEDIA_PLAYER_COMMAND_VOLUME_DOWN:
+      return "VOLUME_DOWN";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+const char *media_player_file_type_to_string(MediaFileType file_type) {
+  switch (file_type) {
+    // case MediaFileType::NONE:
+    //   return "unknown";
+    case MediaFileType::FLAC:
+      return "FLAC";
+    case MediaFileType::MP3:
+      return "MP3";
+    case MediaFileType::WAV:
+      return "WAV";
+    default:
+      return "unknonw";
+  }
+}
+
+void MediaPlayerCall::validate_() {
+  if (this->media_url_.has_value()) {
+    if (this->command_.has_value()) {
+      ESP_LOGW(TAG, "MediaPlayerCall: Setting both command and media_url is not needed.");
+      this->command_.reset();
+    }
+  }
+  if (this->volume_.has_value()) {
+    if (this->volume_.value() < 0.0f || this->volume_.value() > 1.0f) {
+      ESP_LOGW(TAG, "MediaPlayerCall: Volume must be between 0.0 and 1.0.");
+      this->volume_.reset();
+    }
+  }
+}
+
+void MediaPlayerCall::perform() {
+  ESP_LOGD(TAG, "'%s' - Setting", this->parent_->get_name().c_str());
+  this->validate_();
+  if (this->command_.has_value()) {
+    const char *command_s = media_player_command_to_string(this->command_.value());
+    ESP_LOGD(TAG, "  Command: %s", command_s);
+  }
+  if (this->media_url_.has_value()) {
+    ESP_LOGD(TAG, "  Media URL: %s", this->media_url_.value().c_str());
+  }
+  if (this->volume_.has_value()) {
+    ESP_LOGD(TAG, "  Volume: %.2f", this->volume_.value());
+  }
+  if (this->announcement_.has_value()) {
+    ESP_LOGD(TAG, " Announcement: %s", this->announcement_.value() ? "yes" : "no");
+  }
+  this->parent_->control(*this);
+}
+
+MediaPlayerCall &MediaPlayerCall::set_command(MediaPlayerCommand command) {
+  this->command_ = command;
+  return *this;
+}
+MediaPlayerCall &MediaPlayerCall::set_command(optional<MediaPlayerCommand> command) {
+  this->command_ = command;
+  return *this;
+}
+MediaPlayerCall &MediaPlayerCall::set_command(const std::string &command) {
+  if (str_equals_case_insensitive(command, "PLAY")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_PLAY);
+  } else if (str_equals_case_insensitive(command, "PAUSE")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_PAUSE);
+  } else if (str_equals_case_insensitive(command, "STOP")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_STOP);
+  } else if (str_equals_case_insensitive(command, "MUTE")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_MUTE);
+  } else if (str_equals_case_insensitive(command, "UNMUTE")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_UNMUTE);
+  } else if (str_equals_case_insensitive(command, "TOGGLE")) {
+    this->set_command(MEDIA_PLAYER_COMMAND_TOGGLE);
   } else {
-    this->set_volume_(0.0f, false);
+    ESP_LOGW(TAG, "'%s' - Unrecognized command %s", this->parent_->get_name().c_str(), command.c_str());
   }
-  this->muted_ = true;
-}
-void I2SAudioMediaPlayer::unmute_() {
-  if (this->mute_pin_ != nullptr) {
-    this->mute_pin_->digital_write(false);
-  } else {
-    this->set_volume_(this->volume, false);
-  }
-  this->muted_ = false;
-}
-void I2SAudioMediaPlayer::set_volume_(float volume, bool publish) {
-  // Implémentez la logique pour régler le volume ici
-  if (publish)
-    this->volume = volume;
+  return *this;
 }
 
-void I2SAudioMediaPlayer::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up Audio...");
-  this->state = media_player::MEDIA_PLAYER_STATE_IDLE;
+MediaPlayerCall &MediaPlayerCall::set_media_url(const std::string &media_url) {
+  this->media_url_ = media_url;
+  return *this;
 }
 
-void I2SAudioMediaPlayer::loop() {
-  switch (this->i2s_state_) {
-    case I2S_STATE_STARTING:
-      this->start_();
-      break;
-    case I2S_STATE_RUNNING:
-      this->play_();
-      break;
-    case I2S_STATE_STOPPING:
-      this->stop_();
-      break;
-    case I2S_STATE_STOPPED:
-      break;
-  }
+MediaPlayerCall &MediaPlayerCall::set_local_media_file(MediaFile *media_file) {
+  this->media_file_ = media_file;
+  return *this;
 }
 
-void I2SAudioMediaPlayer::play_() {
-  // Implémentez la logique pour lire l'audio ici
-  if ((this->state == media_player::MEDIA_PLAYER_STATE_PLAYING ||
-       this->state == media_player::MEDIA_PLAYER_STATE_ANNOUNCING)) {
-    this->stop();
-  }
+MediaPlayerCall &MediaPlayerCall::set_volume(float volume) {
+  this->volume_ = volume;
+  return *this;
 }
 
-void I2SAudioMediaPlayer::start() { this->i2s_state_ = I2S_STATE_STARTING; }
-void I2SAudioMediaPlayer::start_() {
-  if (!this->parent_->try_lock()) {
-    return;  // Waiting for another i2s to return lock
-  }
-
-  i2s_pin_config_t pin_config = this->parent_->get_pin_config();
-  pin_config.data_out_num = this->dout_pin_;
-  i2s_set_pin(this->parent_->get_port(), &pin_config);
-
-  if (this->mute_pin_ != nullptr) {
-    this->mute_pin_->setup();
-    this->mute_pin_->digital_write(false);
-  }
-
-  this->i2s_state_ = I2S_STATE_RUNNING;
-  this->high_freq_.start();
-  this->set_volume_(this->volume, false);
-
-  if (this->current_url_.has_value()) {
-    // Implémentez la logique pour lire l'URL audio ici
-    this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
-    if (this->is_announcement_) {
-      this->state = media_player::MEDIA_PLAYER_STATE_ANNOUNCING;
-    }
-    this->publish_state();
-  }
-}
-void I2SAudioMediaPlayer::stop() {
-  if (this->i2s_state_ == I2S_STATE_STOPPED) {
-    return;
-  }
-  if (this->i2s_state_ == I2S_STATE_STARTING) {
-    this->i2s_state_ = I2S_STATE_STOPPED;
-    return;
-  }
-  this->i2s_state_ = I2S_STATE_STOPPING;
-}
-void I2SAudioMediaPlayer::stop_() {
-  this->current_url_ = {};
-  this->parent_->unlock();
-  this->i2s_state_ = I2S_STATE_STOPPED;
-
-  this->high_freq_.stop();
-  this->state = media_player::MEDIA_PLAYER_STATE_IDLE;
-  this->publish_state();
-  this->is_announcement_ = false;
+MediaPlayerCall &MediaPlayerCall::set_announcement(bool announce) {
+  this->announcement_ = announce;
+  return *this;
 }
 
-media_player::MediaPlayerTraits I2SAudioMediaPlayer::get_traits() {
-  auto traits = media_player::MediaPlayerTraits();
-  traits.set_supports_pause(true);
-  return traits;
-};
-
-void I2SAudioMediaPlayer::dump_config() {
-  ESP_LOGCONFIG(TAG, "Audio:");
-  if (this->is_failed()) {
-    ESP_LOGCONFIG(TAG, "Audio failed to initialize!");
-    return;
-  }
-  ESP_LOGCONFIG(TAG, "  External DAC channels: %d", this->external_dac_channels_);
-  ESP_LOGCONFIG(TAG, "  I2S DOUT Pin: %d", this->dout_pin_);
-  LOG_PIN("  Mute Pin: ", this->mute_pin_);
+void MediaPlayer::add_on_state_callback(std::function<void()> &&callback) {
+  this->state_callback_.add(std::move(callback));
 }
 
-}  // namespace i2s_audio
+void MediaPlayer::publish_state() { this->state_callback_.call(); }
+
+}  // namespace media_player
 }  // namespace esphome
-
-#endif  // USE_ESP32
