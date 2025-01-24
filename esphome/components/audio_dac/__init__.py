27dcea79b957f9be0a4736a0db0090cc25455eff
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 02:38:28 +0100

    Update __init__.py

--- esphome/components/audio_dac/__init__.py
+++ esphome/components/audio_dac/__init__.py
@@ -5,9 +5,6 @@ from esphome.automation import maybe_simple_id
 from esphome.core import coroutine_with_priority
 from esphome.const import CONF_ID, CONF_VOLUME
 
-# Ajoutez ces imports supplémentaires
-from esphome.components import speaker
-
 CODEOWNERS = ["@youkorr"]
 IS_PLATFORM_COMPONENT = True
 
@@ -50,12 +47,15 @@ async def audio_dac_set_volume_to_code(config, action_id, template_arg, args):
 
     return var
 
-# Enregistrement du composant i2s_audio_speaker
-i2s_audio_speaker_ns = cg.esphome_ns.namespace("i2s_audio_speaker")
-I2SAudioSpeaker = i2s_audio_speaker_ns.class_("I2SAudioSpeaker", speaker.Speaker, cg.Component)
+# Utilisez un import différé pour éviter la dépendance circulaire
+def register_speaker_class():
+    from esphome.components import speaker
+    i2s_audio_speaker_ns = cg.esphome_ns.namespace("i2s_audio_speaker")
+    I2SAudioSpeaker = i2s_audio_speaker_ns.class_("I2SAudioSpeaker", speaker.Speaker, cg.Component)
+    return I2SAudioSpeaker
 
-CONFIG_SCHEMA_I2S_AUDIO_SPEAKER = speaker.SPEAKER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(I2SAudioSpeaker),
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(register_speaker_class()),
     cv.Required("dout_pin"): cv.int_,
     cv.Optional("sample_rate", default=16000): cv.int_,
     cv.Optional("bits_per_sample", default=16): cv.int_,
@@ -72,24 +72,8 @@ async def to_code_i2s_audio_speaker(config):
     cg.add(var.set_bits_per_sample(config["bits_per_sample"]))
     cg.add(var.set_dac_type(config["dac_type"]))
 
-# Enregistrement du composant es8311
-es8311_ns = cg.esphome_ns.namespace("es8311")
-ES8311Component = es8311_ns.class_("ES8311Component", cg.Component)
-
-CONFIG_SCHEMA_ES8311 = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ES8311Component),
-    cv.Required("i2c_address"): cv.i2c_address,
-}).extend(cv.COMPONENT_SCHEMA)
-
-async def to_code_es8311(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
-    cg.add(var.set_i2c_address(config["i2c_address"]))
-
 @coroutine_with_priority(100.0)
 async def to_code(config):
     cg.add_define("USE_AUDIO_DAC")
     cg.add_global(audio_dac_ns.using)
-    await to_code_i2s_audio_speaker(config)  # Ajoutez cette ligne
-    await to_code_es8311(config)  # Ajoutez cette ligne si vous utilisez es8311
+    await to_code_i2s_audio_speaker(config)
