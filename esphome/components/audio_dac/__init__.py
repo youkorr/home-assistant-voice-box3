e7d8138b5026cefb88381ddad107a8417084c9e7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-24 02:12:43 +0100

    Update __init__.py

--- esphome/components/audio_dac/__init__.py
+++ esphome/components/audio_dac/__init__.py
@@ -5,6 +5,9 @@ from esphome.automation import maybe_simple_id
 from esphome.core import coroutine_with_priority
 from esphome.const import CONF_ID, CONF_VOLUME
 
+# Ajoutez ces imports supplémentaires
+from esphome.components import speaker
+
 CODEOWNERS = ["@youkorr"]
 IS_PLATFORM_COMPONENT = True
 
@@ -14,7 +17,6 @@ AudioDac = audio_dac_ns.class_("AudioDac")
 MuteOffAction = audio_dac_ns.class_("MuteOffAction", automation.Action)
 MuteOnAction = audio_dac_ns.class_("MuteOnAction", automation.Action)
 SetVolumeAction = audio_dac_ns.class_("SetVolumeAction", automation.Action)
-
 
 MUTE_ACTION_SCHEMA = maybe_simple_id(
     {
@@ -30,13 +32,11 @@ SET_VOLUME_ACTION_SCHEMA = cv.maybe_simple_value(
     key=CONF_VOLUME,
 )
 
-
 @automation.register_action("audio_dac.mute_off", MuteOffAction, MUTE_ACTION_SCHEMA)
 @automation.register_action("audio_dac.mute_on", MuteOnAction, MUTE_ACTION_SCHEMA)
 async def audio_dac_mute_action_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     return cg.new_Pvariable(action_id, template_arg, paren)
-
 
 @automation.register_action(
     "audio_dac.set_volume", SetVolumeAction, SET_VOLUME_ACTION_SCHEMA
@@ -50,8 +50,30 @@ async def audio_dac_set_volume_to_code(config, action_id, template_arg, args):
 
     return var
 
+# Ajoutez cette section pour enregistrer le composant i2s_audio_speaker
+i2s_audio_speaker_ns = cg.esphome_ns.namespace("i2s_audio_speaker")
+I2SAudioSpeaker = i2s_audio_speaker_ns.class_("I2SAudioSpeaker", speaker.Speaker, cg.Component)
+
+CONFIG_SCHEMA = speaker.SPEAKER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(I2SAudioSpeaker),
+    cv.Required("dout_pin"): cv.int_,
+    cv.Optional("sample_rate", default=16000): cv.int_,
+    cv.Optional("bits_per_sample", default=16): cv.int_,
+    cv.Optional("dac_type", default="external"): cv.string,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code_i2s_audio_speaker(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await speaker.register_speaker(var, config)
+
+    cg.add(var.set_dout_pin(config["dout_pin"]))
+    cg.add(var.set_sample_rate(config["sample_rate"]))
+    cg.add(var.set_bits_per_sample(config["bits_per_sample"]))
+    cg.add(var.set_dac_type(config["dac_type"]))
 
 @coroutine_with_priority(100.0)
 async def to_code(config):
     cg.add_define("USE_AUDIO_DAC")
     cg.add_global(audio_dac_ns.using)
+    await to_code_i2s_audio_speaker(config)  # Ajoutez cette ligne
