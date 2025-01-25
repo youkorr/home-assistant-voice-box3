ab6fb79101163222202ab567d928051a303948aa
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-25 01:36:59 +0100

    Update __init__.py

--- esphome/components/es8311/__init__.py
+++ esphome/components/es8311/__init__.py
@@ -1,15 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, audio_dac
+from esphome.components import i2c
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["audio_dac"]
+AUTO_LOAD = []
 
 es8311_ns = cg.esphome_ns.namespace("es8311")
-ES8311 = es8311_ns.class_("ES8311Component", audio_dac, i2c.I2CDevice, cg.Component)
+audio_dac_ns = cg.esphome_ns.namespace("audio_dac")
 
-CONFIG_SCHEMA = audio_dac.AUDIO_DAC_SCHEMA.extend({
+# Définissez AudioDac dans le namespace audio_dac
+AudioDac = audio_dac_ns.class_("AudioDac")
+
+# Créez ES8311Component en héritant de AudioDac, I2CDevice et Component
+ES8311 = es8311_ns.class_("ES8311Component", AudioDac, i2c.I2CDevice, cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ES8311),
 }).extend(i2c.i2c_device_schema(0x18)).extend(cv.COMPONENT_SCHEMA)
 
@@ -17,3 +23,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
+
