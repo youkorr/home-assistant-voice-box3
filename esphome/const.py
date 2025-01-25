591611702103101af10bea3393ab141d75c1c1a2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-01-25 01:59:35 +0100

    Add files via upload

---
+++ esphome/const.py
@@ -0,0 +1,7 @@
+CONF_MESSAGE = "message"
+CONF_METHANE = "methane"
+CONF_METHOD = "method"
+CONF_MIC_GAIN = "mic_gain"
+CONF_MICROPHONE = "microphone"
+CONF_MIN_BRIGHTNESS = "min_brightness"
+CONF_MIN_COOLING_OFF_TIME = "min_cooling_off_time"
