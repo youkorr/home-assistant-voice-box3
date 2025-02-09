6f23823ea26400550511000e70f50e3b9609d14b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-09 21:41:08 +0100

    Update __init__.py

--- esphome/components/media_player/__init__.py
+++ esphome/components/media_player/__init__.py
@@ -2,19 +2,26 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_NAME
 from esphome.components import i2s_audio
-from esphome.components.media_player import MediaPlayerDevice  # Change this line
 
 CODEOWNERS = ["@youkorr"]
 DEPENDENCIES = ["i2s_audio"]
 AUTO_LOAD = ["i2s_audio"]
 
+# Définir le namespace
 i2s_audio_ns = cg.esphome_ns.namespace("i2s_audio")
+media_player_ns = cg.esphome_ns.namespace('media_player')
+
+# Créer la classe de base MediaPlayerDevice
+MediaPlayerDevice = media_player_ns.class_('MediaPlayerDevice', cg.Component)
+
+# Définir la classe I2SAudioMediaPlayer
 I2SAudioMediaPlayer = i2s_audio_ns.class_(
-    "I2SAudioMediaPlayer", MediaPlayerDevice, cg.Component  # Changed this line
+    "I2SAudioMediaPlayer", MediaPlayerDevice
 )
 
+# Schéma de configuration
 CONFIG_SCHEMA = cv.All(
-    MediaPlayerDevice.schema().extend({  # Changed this line
+    cv.Schema({
         cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
         cv.Required("dac_type"): cv.one_of("external", "internal", lower=True),
         cv.Optional("sample_rate", default=44100): cv.int_range(min=8000, max=192000),
@@ -26,7 +33,7 @@ CONFIG_SCHEMA = cv.All(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await MediaPlayerDevice.register_media_player(var, config)  # Changed this line
+    
     cg.add(var.set_dac_type(config["dac_type"]))
     cg.add(var.set_sample_rate(config["sample_rate"]))
     cg.add(var.set_mode(0 if config["mode"] == "mono" else 1))
