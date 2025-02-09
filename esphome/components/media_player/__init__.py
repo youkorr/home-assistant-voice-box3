00e521d6f6a9c3467b0a3a50fa5c64591201a628
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-09 21:22:09 +0100

    Update __init__.py

--- esphome/components/media_player/__init__.py
+++ esphome/components/media_player/__init__.py
@@ -1,24 +1,40 @@
-# Ajoutez dans la partie validation
-CONFIG_SCHEMA = cv.All(
-    media_player.MEDIA_PLAYER_SCHEMA.extend({
-        cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
-        cv.Optional("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),
-    }).extend(cv.COMPONENT_SCHEMA),
-    cv.has_at_least_one_key(),
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import media_player, i2s_audio
+from esphome.const import CONF_ID, CONF_NAME
+
+CODEOWNERS = ["@youkorr"]
+DEPENDENCIES = ["i2s_audio"]
+
+AUTO_LOAD = ["i2s_audio"]
+
+i2s_audio_ns = cg.esphome_ns.namespace("i2s_audio")
+I2SAudioMediaPlayer = i2s_audio_ns.class_(
+    "I2SAudioMediaPlayer", media_player.MediaPlayer, cg.Component
 )
 
-# Modifiez la fonction to_code
-@coroutine_with_priority(100.0)
+CONFIG_SCHEMA = cv.All(
+    media_player.MEDIA_PLAYER_DEVICE_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
+            cv.Required("dac_type"): cv.one_of("external", "internal", lower=True),
+            cv.Optional("sample_rate", default=44100): cv.int_range(min=8000, max=192000),
+            cv.Optional("mode", default="mono"): cv.one_of("mono", "stereo", lower=True),
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    cv.only_with_arduino,
+)
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await media_player.register_media_player(var, config)
+
+    cg.add(var.set_dac_type(config["dac_type"]))
+    cg.add(var.set_sample_rate(config["sample_rate"]))
+    cg.add(var.set_mode(0 if config["mode"] == "mono" else 1))
     
-    # Récupération automatique si ID non spécifié
-    if "i2s_audio_id" in config:
-        i2s_audio_var = await cg.get_variable(config["i2s_audio_id"])
-    else:
-        # Utilise le premier composant I2S trouvé
-        i2s_audio_var = await cg.get_variable(i2s_audio.I2SAudioComponent)
-    
-    cg.add(var.set_i2s_audio(i2s_audio_var))
+    # Configuration I2S
+    if config["dac_type"] == "external":
+        cg.add_library("esphome/ESP32-audioI2S", "2.0.7")
+        cg.add_build_flag("-DUSE_I2S_EXTERNAL_DAC")
