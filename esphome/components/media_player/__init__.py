2405ef6bae54a06482c3134396d3a54a3c6ded98
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-09 21:16:22 +0100

    Update __init__.py

--- esphome/components/media_player/__init__.py
+++ esphome/components/media_player/__init__.py
@@ -1,30 +1,24 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_NAME
-from esphome.components import i2s_audio
-from esphome.core import coroutine_with_priority
+# Ajoutez dans la partie validation
+CONFIG_SCHEMA = cv.All(
+    media_player.MEDIA_PLAYER_SCHEMA.extend({
+        cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
+        cv.Optional("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),
+    }).extend(cv.COMPONENT_SCHEMA),
+    cv.has_at_least_one_key(),
+)
 
-CODEOWNERS = ["@youkorr"]
-
-IS_PLATFORM_COMPONENT = True
-
-media_player_ns = cg.esphome_ns.namespace("media_player")
-MediaPlayer = media_player_ns.class_("MediaPlayer", cg.Component)
-
-# Configuration schema MODIFIÉE
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MediaPlayer),
-    cv.Required(CONF_NAME): cv.string,
-    # Utilisez directement l'ID du composant i2s_audio sans référence de classe
-    cv.Required("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),  # <-- Changement clé ici
-}).extend(cv.COMPONENT_SCHEMA)
-
+# Modifiez la fonction to_code
 @coroutine_with_priority(100.0)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add(var.set_name(config[CONF_NAME]))
+    await media_player.register_media_player(var, config)
     
-    # Lier i2s_audio via son ID générique
-    i2s_audio_var = await cg.get_variable(config["i2s_audio_id"])
+    # Récupération automatique si ID non spécifié
+    if "i2s_audio_id" in config:
+        i2s_audio_var = await cg.get_variable(config["i2s_audio_id"])
+    else:
+        # Utilise le premier composant I2S trouvé
+        i2s_audio_var = await cg.get_variable(i2s_audio.I2SAudioComponent)
+    
     cg.add(var.set_i2s_audio(i2s_audio_var))
